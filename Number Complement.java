67ff9f88d9be0612d9c742f2d73512e3705fa2cd
hojason117 <hojason117@gmail.com>
2017-02-02 23:47:10 -0500

    Add files via upload

---
+++ Number Complement.java
@@ -0,0 +1,11 @@
+public class Solution {
+	public int findComplement(int num) {
+		int zeroCount = Integer.numberOfLeadingZeros(num);
+		String s = "";
+		
+        for(int i = 0; i < 32-zeroCount; i++)
+        	s += '1';
+        
+        return num ^ Integer.parseInt(s, 2);
+    }
+}
