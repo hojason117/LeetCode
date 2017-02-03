06e3a3c68445aa29c90371bfe11cde148a1c3c54
hojason117 <hojason117@gmail.com>
2017-02-03 00:42:33 -0500

    Add files via upload

---
+++ Reverse String.java
@@ -0,0 +1,18 @@
+public class Solution {
+	 public String reverseString(String s) {
+        char[] ch = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        char temp;
+        
+        while(i < j) {
+        	temp = ch[i];
+        	ch[i] = ch[j];
+        	ch[j] = temp;
+        	i++;
+        	j--;
+        }
+        
+        return new String(ch);
+    }
+}
