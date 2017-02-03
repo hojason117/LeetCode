7583971401383b3296eb067a8ceeb7c35037009c
hojason117 <hojason117@gmail.com>
2017-02-03 00:51:40 -0500

    Add files via upload

---
+++ Max Consecutive Ones.java
@@ -0,0 +1,21 @@
+public class Solution {
+	public int findMaxConsecutiveOnes(int[] nums) {
+		int len = 0;
+		int temp = 0;
+		
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] == 1)
+        		temp++;
+        	else {
+        		if(temp > len)
+        			len = temp;
+        		temp = 0;
+        	}
+        }
+        
+        if(temp > len)
+			len = temp;
+        
+        return len;
+    }
+}
