bb9d37409dd00d1e9496c8aed279327188252032
hojason117 <hojason117@gmail.com>
2017-02-04 18:41:51 -0500

    Update Minimum Depth of Binary Tree.java

--- Minimum Depth of Binary Tree.java
+++ Minimum Depth of Binary Tree.java
@@ -3,16 +3,16 @@ public class Solution {
 		if(root == null)
 			return 0;
 		
-        if(root.left == null && root.right == null)
-        	return 1;
-        else if(root.left == null && root.right != null)
-        	return minDepth(root.right) + 1;
-        else if(root.left != null && root.right == null)
-        	return minDepth(root.left) + 1;
-        else {
-        	int left = minDepth(root.left);
-        	int right = minDepth(root.right);
-        	return (left < right) ? left+1 : right+1;
-        }
-    }
+		if(root.left == null && root.right == null)
+			return 1;
+		else if(root.left == null && root.right != null)
+			return minDepth(root.right) + 1;
+		else if(root.left != null && root.right == null)
+			return minDepth(root.left) + 1;
+		else {
+			int left = minDepth(root.left);
+			int right = minDepth(root.right);
+			return (left < right) ? left+1 : right+1;
+		}
+	}
 }
