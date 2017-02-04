8b4d15792f7efc25179c4ba0a0381fd814d7483b
hojason117 <hojason117@gmail.com>
2017-02-04 16:42:57 -0500

    Update Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -1,18 +1,18 @@
 public class Solution {
 	public String reverseString(String s) {
-        char[] ch = s.toCharArray();
-        int i = 0;
-        int j = s.length() - 1;
-        char temp;
+        	char[] ch = s.toCharArray();
+        	int i = 0;
+        	int j = s.length() - 1;
+        	char temp;
         
-        while(i < j) {
-        	temp = ch[i];
-        	ch[i] = ch[j];
-        	ch[j] = temp;
-        	i++;
-        	j--;
-        }
+		while(i < j) {
+			temp = ch[i];
+			ch[i] = ch[j];
+			ch[j] = temp;
+			i++;
+			j--;
+		}
         
-        return new String(ch);
-    }
+        	return new String(ch);
+    	}
 }
