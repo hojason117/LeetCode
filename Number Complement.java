76ae48e99f188acdafa7f85b06b967cab1e242e4
hojason117 <hojason117@gmail.com>
2017-02-04 16:42:15 -0500

    Update Number Complement.java

--- Number Complement.java
+++ Number Complement.java
@@ -3,9 +3,9 @@ public class Solution {
 		int zeroCount = Integer.numberOfLeadingZeros(num);
 		String s = "";
 		
-        for(int i = 0; i < 32-zeroCount; i++)
-        	s += '1';
+        	for(int i = 0; i < 32-zeroCount; i++)
+        		s += '1';
         
-        return num ^ Integer.parseInt(s, 2);
-    }
+        	return num ^ Integer.parseInt(s, 2);
+    	}
 }
