87848eac88ad41362180b96c1753bf6b37dcc38b
hojason117 <hojason117@gmail.com>
2017-02-04 16:41:13 -0500

    Update Max Consecutive Ones.java

--- Max Consecutive Ones.java
+++ Max Consecutive Ones.java
@@ -3,19 +3,19 @@ public class Solution {
 		int len = 0;
 		int temp = 0;
 		
-        for(int i = 0; i < nums.length; i++) {
-        	if(nums[i] == 1)
-        		temp++;
-        	else {
-        		if(temp > len)
-        			len = temp;
-        		temp = 0;
-        	}
-        }
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] == 1)
+				temp++;
+			else {
+				if(temp > len)
+					len = temp;
+				temp = 0;
+			}
+		}
         
-        if(temp > len)
+		if(temp > len)
 			len = temp;
         
-        return len;
-    }
+		return len;
+	}
 }
