b532f000702c31ef6a07132fe3449229a55cc6db
hojason117 <hojason117@gmail.com>
2017-02-04 16:39:09 -0500

    Update Hamming Distance.java

--- Hamming Distance.java
+++ Hamming Distance.java
@@ -1,15 +1,15 @@
 public class Solution {
 	public int hammingDistance(int x, int y) {
-        int temp = x ^ y;
+        	int temp = x ^ y;
         
-        int result = 0;
-        for(int i = 30; i >= 0; i--) {
-        	if(temp / (int)Math.pow(2, i) == 1)  {
-        		temp %= (int)Math.pow(2, i);
-        		result++;
+        	int result = 0;
+		for(int i = 30; i >= 0; i--) {
+			if(temp / (int)Math.pow(2, i) == 1)  {
+				temp %= (int)Math.pow(2, i);
+				result++;
+        		}
         	}
-        }
         
-        return result;
-    }
+        	return result;
+	}
 }
