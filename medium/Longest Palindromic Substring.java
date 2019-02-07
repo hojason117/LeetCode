c352530fee3a4d8032ffaf51bbf130b3d41475e2
Jason Ho <hojason117@gmail.com>
2019-02-06 23:05:27 -0500

    Longest Palindromic Substring

--- medium/Longest Palindromic Substring.java
+++ medium/Longest Palindromic Substring.java
@@ -1,6 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
         String longestResult = "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            String temp = helper(i - 1, i + 1, s, longestResult.length());
+            if(temp != null)
+                longestResult = temp;
+            
+            temp = helper(i, i + 1, s, longestResult.length());
+            if(temp != null)
+                longestResult = temp;
+        }
+        
+        return longestResult;
+    }
+    
+    private String helper(int left, int right, String s, int currentMax) {
+        while(left >= 0 && right < s.length()) {
+            if(s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            else
+                break;
+        }
+        
+        return (right - left - 1 > currentMax) ? s.substring(left + 1, right) : null;
+    }
+}
+
+// Brute Force
+class Solution {
+    public String longestPalindrome(String s) {
+        String longestResult = "";
         for(int j = 0; j < s.length(); j++) {
             if(s.length() - j < longestResult.length())
                 return longestResult;
--- medium/Longest Substring Without Repeating Characters.java
+++ medium/Longest Substring Without Repeating Characters.java
@@ -1,3 +1,4 @@
+// Sliding Window
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int max = 0, temp = 0, start = 0, end = 0;
@@ -16,6 +17,32 @@ class Solution {
             
             if(temp > max)
                 max = temp;
+        }
+        
+        return max;
+    }
+}
+
+// Brute Force
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(max > s.length() - i)
+                break;
+            
+            int count = 0;
+            HashSet<Character> seen = new HashSet<Character>();
+            
+            int pos = i;
+            while(pos < s.length() && !seen.contains(s.charAt(pos))) {
+                seen.add(s.charAt(pos++));
+                count++;
+            }
+            
+            if(count > max)
+                max = count;
         }
         
         return max;
