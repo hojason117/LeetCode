c7fcfd95f69b7429a57cbd7f826cc4074c9878f9
Jason Ho <hojason117@gmail.com>
2019-02-20 12:40:49 -0500

    Reverse Linked List

--- easy/Reverse Linked List.java
+++ easy/Reverse Linked List.java
@@ -1,19 +1,57 @@
-public class Solution {
-	public ListNode reverseList(ListNode head) {
-		if(head == null)
-			return null;
-		
-		ListNode temp1;
-		ListNode temp2 = head;
-		head = null;
-		
-		while(temp2 != null) {
-			temp1 = temp2.next;
-			temp2.next = head;
-			head = temp2;
-			temp2 = temp1;
-		}
-		
-		return head;
-	}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// Recursively 1
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return helper(null, head);
+    }
+    
+    private ListNode helper(ListNode previous, ListNode current) {
+        if(current == null)
+            return previous;
+        
+        ListNode temp = current.next;
+        current.next = previous;
+        previous = current;
+        current = temp;
+        
+        return helper(previous, current);
+    }
+}
+
+// Recursively 2
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return reversed;
+    }
+}
+
+// Iteratively
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pointer = head;
+        ListNode previous = null;
+        while(pointer != null) {
+            ListNode temp = pointer.next;
+            pointer.next = previous;
+            previous = pointer;
+            pointer = temp;
+        }
+        
+        return previous;
+    }
 }
