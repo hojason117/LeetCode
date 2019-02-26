39b16257890a6d08d3c55a2408a354560aabb461
Jason Ho <hojason117@gmail.com>
2019-02-25 21:20:15 -0500

    Move Zeroes

--- easy/Move Zeroes.java
+++ easy/Move Zeroes.java
@@ -1,14 +1,34 @@
-public void moveZeroes(int[] nums) {
-	int zeroCount = 0;
-	int p = 0;
-	
-	for(int i = 0; i < nums.length; i++) {
-		if(nums[i] == 0)
-			zeroCount++;
-		else
-			nums[p++] = nums[i];
+// counting zeros
+class Solution {
+	public void moveZeroes(int[] nums) {
+		int zeroCount = 0;
+		int p = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] == 0)
+				zeroCount++;
+			else
+				nums[p++] = nums[i];
+		}
+		
+		for(int j = 0; j < zeroCount; j++)
+			nums[p++] = 0;
 	}
-	
-	for(int j = 0; j < zeroCount; j++)
-		nums[p++] = 0;
+}
+
+// swapping
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int firstZero = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0)
+                swap(nums, i, firstZero++);
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
