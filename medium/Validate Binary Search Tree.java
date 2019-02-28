15debcab6f2b24a3b17fb297d6b883407bee4bc3
Jason Ho <hojason117@gmail.com>
2019-02-28 17:11:53 -0500

    Validate Binary Search Tree

--- medium/Validate Binary Search Tree.java
+++ medium/Validate Binary Search Tree.java
@@ -7,29 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return validate(root, null, null);
     }
     
-    private boolean validate(TreeNode root, Integer lower, Integer upper) {
-        if (root == null)
+    private boolean validate(TreeNode target, Integer min, Integer max) {
+        if(target == null)
             return true;
         
-        if(root.left != null) {
-            if(root.left.val >= root.val || (lower != null && root.left.val <= lower))
-                return false;
-        }
-        if(root.right != null) {
-            if(root.right.val <= root.val || (upper != null && root.right.val >= upper))
-                return false;
-        }
-        
-        if(root.left != null && !validate(root.left, lower, root.val))
-            return false;
-        if(root.right != null && !validate(root.right, root.val, upper))
+        if((min != null && target.val <= min) || (max != null && target.val >= max))
             return false;
         
-        return true;
+        return validate(target.left, min, target.val) && validate(target.right, target.val, max);
     }
 }
